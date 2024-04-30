27dda57e3c5d41be4401376ee7b53c9bad10a755
waiyanhtet1 <waiyanhtet1661390@gmail.com>
2024-04-30 16:06:32 +0630

    setup: erros and accuracy helpers functions

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,3 +1,20 @@
 export const formatPercentage = (percentage: number) => {
   return percentage.toFixed(0) + "%";
 };
+
+export const countErrors = (actual: string, expected: string) => {
+  const expectedCharacter = expected.split("");
+
+  return expectedCharacter.reduce((erros, expectedChar, i) => {
+    if (actual[i] !== expectedChar) erros++;
+    return erros;
+  }, 0);
+};
+
+export const calculateAccuracyPercentage = (erros: number, total: number) => {
+  if (total > 0) {
+    const corrects = total - erros;
+    return (corrects / total) * 100;
+  }
+  return 0;
+};
